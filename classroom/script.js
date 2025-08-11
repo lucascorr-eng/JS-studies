cb6464e85e414a722c99b7df54acad51db58f687
Lucas Corrêa - popOS <lucas.engsoftwareee@gmail.com>
2025-08-11 13:32:06 -0300

    how add coments in the code

--- classroom/script.js
+++ classroom/script.js
@@ -1,2 +1,11 @@
 console.log("Hello World")
-console.log("Essa mensagem é do JS conectado com o HTML")
+
+console.log("vamos aprender a usar os comentários no CSS")
+
+//isso é um comentário no JS - sera ignorado pelo código
+// é bom para comentários de uma linha 
+
+/*
+é usado para criar comentários de mais de uma linha 
+e desenvolver um raciocíonio maior
+*/
