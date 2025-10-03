2c179c8b48d8abe071dc6b6c295b06d057f8dda3
Lucas Mint <lucas.engsoftwareee@gmail.com>
2025-10-03 10:08:34 -0300

    my frist function

--- classroom/script.js
+++ classroom/script.js
@@ -1,16 +1,6 @@
-//variável com um valor fixo - ela não vai alterar
-//Uma constante cria uma variável cujo valor é fixo (não pode ser alterado)
+function message() {
+  console.log("Helllo world!")
+  console.log("Olá, eutenho uma função")
+}
 
-const number = 42
-console.log(number)
-
-/*
-number = 55
-console.log(number)
-
-Gera um erro porque o valor foi alterado
-
-erro do console:
-Uncaught TypeError: Assignment to constant variable.
-
-*/
+message ()
