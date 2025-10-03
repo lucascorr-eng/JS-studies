91c07d7a96420c8cabb8c1a1149917d53fb0cd1d
Lucas Mint <lucas.engsoftwareee@gmail.com>
2025-10-03 10:32:22 -0300

    Arugmentos e parametros

--- classroom/script.js
+++ classroom/script.js
@@ -1,6 +1,21 @@
-function message() {
-  console.log("Helllo world!")
-  console.log("Olá, eutenho uma função")
+// function message(username) {
+//   console.log("Olá", username)
+// }
+
+// message("Lucas");
+// message("Teste");
+
+// function sum(a, b) {
+//   console.log(a + b)
+// }
+
+// sum(10, 20)
+
+
+// Definindo um valor (argumento) padrão
+
+function joinText(text1, text2 = "teste 2", text3 = "teste 3") {
+  console.log(text1, text2, text3)
 }
 
-message ()
+joinText("Teste", "teste dois")
